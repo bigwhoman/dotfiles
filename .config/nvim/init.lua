c25430d81b8758695dcb5c19563a363f90d94cbd
Hooman <hm.keshvari@gmail.com>
2025-07-11 12:47:34 -0400

    changed filemanager to thunar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,10 @@
-require("core.options")
-require("config.lazy")
-require("core.keymaps")
+
+if vim.g.vscode then
+    -- VSCode extension
+    -- Only include essential settings here
+else
+    require("core.options")
+    require("config.lazy")
+    require("core.keymaps")
+end
+
