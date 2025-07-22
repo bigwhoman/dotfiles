4ae4a0ca9f23f36b292a81ec7ce47e5d6ce098b8
Hooman <hm.keshvari@gmail.com>
2025-07-22 18:17:51 -0400

    a bunch of changes to nvim

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -15,3 +15,10 @@ keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "New tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close Tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next Tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Prev Tab" })
+
+-- Add this to your init.lua or LSP config
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
