f322f83f1ae7cd3720f79eee9d8b5156f665221a
bigwhoman <hm.keshvari@gmail.com>
2025-10-21 10:13:23 -0400

    added a bunch of fonts and changed monitor for laptop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 # Auto-start tmux
 if status is-interactive
     and not set -q TMUX
-    and not string match -q "*ssh*" $SSH_CONNECTION
-    exec tmux new-session -A -s main
+    exec tmux
 end
 alias vim='nvim'
 function fish_user_key_bindings
